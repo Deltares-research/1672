586dad8159f976c0b13fa190664cc624315078d6
Koen Berends <k.d.berends@utwente.nl>
2021-08-24 17:26:30 +0200

    removed active tag from navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,7 +9,7 @@
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
       <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav class="ml-auto">
-        <b-nav-item active to="/">Home</b-nav-item>
+        <b-nav-item to="/">Home</b-nav-item>
         <b-nav-item to="/project">Project</b-nav-item>
         <b-nav-item to="/blog">Blog</b-nav-item>
         <b-nav-item to="/contact">Contact</b-nav-item>
